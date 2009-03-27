49c60178cb972e4a5ce0995dcbcbaac8d905a008
Cristi Duma <cristi.duma@aissac.ro>
2009-03-27 17:06:01 +0200

    fixed custom_field (stereotype) not found

--- lib/stereotype/page_extensions.rb
+++ lib/stereotype/page_extensions.rb
@@ -34,7 +34,8 @@ module Stereotype
       def new_with_defaults_with_stereotype(config = Radiant::Config)
         page = new
       
-        name = CustomField.find(:first, :conditions => { :name => "stereotype", :page_id => page.parent_id}).value
+        custom_field = CustomField.find(:first, :conditions => { :name => "stereotype", :page_id => page.parent_id})
+        name = custom_field && custom_field.value
       
         if name
           parts_and_filters = config["stereotype.#{name}.parts"].blank? ? config["defaults.page.parts"].to_s.strip.split(',') : config["stereotype.#{name}.parts"].to_s.strip.split(',')
